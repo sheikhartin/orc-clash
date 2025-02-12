b9ee599fede9a8d1d6735330153353d56122e2ea
Artin <sheikhartin@gmail.com>
2025-02-12 13:08:34 +0330

    To using `this`

--- src/Core/StateMachine.cs
+++ src/Core/StateMachine.cs
@@ -19,7 +19,7 @@ public partial class StateMachine : Node {
     public IState CurrentState;
     public readonly Dictionary<string, IState> States = new();
 
-    public void AddState(string name, IState state) => States[name] = state;
+    public void AddState(string name, IState state) => this.States[name] = state;
 
     public void ChangeState(string newState) {
         if (string.IsNullOrEmpty(newState)) {
@@ -27,18 +27,18 @@ public partial class StateMachine : Node {
             return;
         }
 
-        if (States.TryGetValue(newState, out IState nextState)) {
-            CurrentState?.Exit();
-            CurrentState = nextState;
-            CurrentState.Enter();
+        if (this.States.TryGetValue(newState, out IState nextState)) {
+            this.CurrentState?.Exit();
+            this.CurrentState = nextState;
+            this.CurrentState.Enter();
         } else {
             GD.PushError($"State '{newState}' not found!");
         }
     }
 
     public void EnterInitialState() {
-        if (!string.IsNullOrEmpty(InitialState)) {
-            ChangeState(InitialState);
+        if (!string.IsNullOrEmpty(this.InitialState)) {
+            ChangeState(this.InitialState);
         } else {
             GD.PushWarning("Initial state is not set in the state machine.");
         }
@@ -46,9 +46,9 @@ public partial class StateMachine : Node {
 
     // public override void _Ready() => EnterInitialState();
 
-    public override void _Process(double delta) => CurrentState?.Process(delta);
+    public override void _Process(double delta) => this.CurrentState?.Process(delta);
 
-    public override void _PhysicsProcess(double delta) => CurrentState?.PhysicsProcess(delta);
+    public override void _PhysicsProcess(double delta) => this.CurrentState?.PhysicsProcess(delta);
 
-    public override void _Input(InputEvent @event) => CurrentState?.HandleInput(@event);
+    public override void _Input(InputEvent @event) => this.CurrentState?.HandleInput(@event);
 }
