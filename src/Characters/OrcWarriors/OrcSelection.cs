37eef10714af88e3fbd7cf56a69c6c2f04fa935d
Artin <sheikhartin@gmail.com>
2025-02-16 20:50:23 +0330

    To add an orc to the scene

---
+++ src/Characters/OrcWarriors/OrcSelection.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+using Godot;
+
+namespace OrcClash.OrcWarriors;
+
+public partial class OrcSelection : Node2D {
+    public enum OrcType {
+        SwampSlasher,
+        JungleAxeman,
+        MeadowCrusher,
+    }
+
+    private static readonly Dictionary<OrcType, string> OrcScenePaths = new() {
+        { OrcType.SwampSlasher, "res://scenes/characters/orc_warriors/swamp_slasher.tscn" },
+        // { OrcType.JungleAxeman, "res://scenes/characters/orc_warriors/jungle_axeman.tscn" },
+        // { OrcType.MeadowCrusher, "res://scenes/characters/orc_warriors/meadow_crusher.tscn" },
+    };
+
+    [Export]
+    public OrcType Type { get; protected set; } = OrcType.SwampSlasher;
+
+    public void SpawnSelectedOrc() {
+        if (!OrcScenePaths.TryGetValue(Type, out var scenePath)) {
+            GD.PushError($"Invalid orc type: {Type}");
+            return;
+        }
+
+        PackedScene orcScene = GD.Load<PackedScene>(scenePath);
+        CharacterBody2D orcInstance = orcScene.Instantiate<CharacterBody2D>();
+
+        GetParent().CallDeferred("add_child", orcInstance);
+        orcInstance.GlobalPosition = GlobalPosition;
+
+        GD.Print($"Spawned {Type} at position {orcInstance.GlobalPosition}");
+    }
+
+    public override void _Ready() => SpawnSelectedOrc();
+}
