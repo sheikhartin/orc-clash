769a2fde6d0835da80b5e78d4ff518d7de8fb9a9
Artin <sheikhartin@gmail.com>
2025-02-19 22:35:20 +0330

    To include the orc in the players' group

--- src/Characters/OrcWarriors/OrcSelection.cs
+++ src/Characters/OrcWarriors/OrcSelection.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 using Godot;
 
-namespace OrcClash.OrcWarriors;
+namespace OrcClash.Characters.OrcWarriors;
 
 public partial class OrcSelection : Node2D {
     public enum OrcType {
@@ -13,24 +13,25 @@ public partial class OrcSelection : Node2D {
 
     private static readonly Dictionary<OrcType, string> OrcScenePaths = new() {
         { OrcType.SwampSlasher, "res://scenes/characters/orc_warriors/swamp_slasher.tscn" },
-        // { OrcType.JungleAxeman, "res://scenes/characters/orc_warriors/jungle_axeman.tscn" },
-        // { OrcType.MeadowCrusher, "res://scenes/characters/orc_warriors/meadow_crusher.tscn" },
-    };
+		// { OrcType.JungleAxeman, "res://scenes/characters/orc_warriors/jungle_axeman.tscn" },
+		// { OrcType.MeadowCrusher, "res://scenes/characters/orc_warriors/meadow_crusher.tscn" },
+	};
 
     [Export]
     public OrcType Type { get; protected set; } = OrcType.SwampSlasher;
 
     public void SpawnSelectedOrc() {
-        if (!OrcScenePaths.TryGetValue(Type, out var scenePath)) {
+        if (!OrcScenePaths.TryGetValue(Type, out string scenePath)) {
             GD.PushError($"Invalid orc type: {Type}");
             return;
         }
 
         PackedScene orcScene = GD.Load<PackedScene>(scenePath);
         CharacterBody2D orcInstance = orcScene.Instantiate<CharacterBody2D>();
+        orcInstance.AddToGroup("players");
 
-        GetParent().CallDeferred("add_child", orcInstance);
-        orcInstance.GlobalPosition = GlobalPosition;
+        base.GetParent().CallDeferred("add_child", orcInstance);
+        orcInstance.GlobalPosition = base.GlobalPosition;
 
         GD.Print($"Spawned {Type} at position {orcInstance.GlobalPosition}");
     }
